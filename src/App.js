755c3ca79edd9ffe126d76ad79b93c73156d67de
Jofre Tomas <jofretomas9@gmail.com>
2024-05-07 12:48:22 -0300

    Criação e estruraçoes das Homepages principais

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./components/pages/Home";
+import Company from "./components/pages/Company";
+import Contact from "./components/pages/Contact";
+import NewProject from "./components/pages/NewProject";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <ul>
+          <li>Home</li>
+          <li>Contato</li>
+          <li></li>
+          <li></li>
+        </ul>
+        <Routes>
+          <Route exact path="/">
+            <Home />
+          </Route>
+
+          <Route path="/company">
+            <Company />
+          </Route>
+
+          <Route path="/contact">
+            <Contact />
+          </Route>
+
+          <Route path="/newproject">
+            <NewProject />
+          </Route>
+        </Routes>
+        <p>Footer</p>
+      </Router>
     </div>
   );
 }
