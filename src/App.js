9f47ca51a4e6e92a946307e3d434644bd770b85d
Jofre Tomas <jofretomas9@gmail.com>
2024-05-07 15:51:01 -0300

    iniciação dos layout  e paginas, inicio processo de estilização das páginas

--- src/App.js
+++ src/App.js
@@ -1,36 +1,28 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import Home from "./components/pages/Home";
 import Company from "./components/pages/Company";
 import Contact from "./components/pages/Contact";
 import NewProject from "./components/pages/NewProject";
+import Container from "./components/layout/Container";
 
 function App() {
   return (
     <div className="App">
       <Router>
-        <ul>
-          <li>Home</li>
-          <li>Contato</li>
-          <li></li>
-          <li></li>
-        </ul>
-        <Routes>
-          <Route exact path="/">
-            <Home />
-          </Route>
-
-          <Route path="/company">
-            <Company />
-          </Route>
-
-          <Route path="/contact">
-            <Contact />
-          </Route>
-
-          <Route path="/newproject">
-            <NewProject />
-          </Route>
-        </Routes>
+        <div>
+          <Link to="/">Inicio</Link>
+          <Link to="/company">Empresa</Link>
+          <Link to="/contact">Contato</Link>
+          <Link to="/newproject">Novo Projeto</Link>
+        </div>
+        <Container className="min-height">
+          <Routes>
+            <Route exact path="/" element={<Home />}></Route>
+            <Route path="/company" element={<Company />}></Route>
+            <Route path="/contact" element={<Contact />}></Route>
+            <Route path="/newproject" element={<NewProject />}></Route>
+          </Routes>
+        </Container>
         <p>Footer</p>
       </Router>
     </div>
