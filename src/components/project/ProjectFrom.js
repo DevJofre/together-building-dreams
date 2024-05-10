7aebe8b9af36df25f48c012b61b78a728793b4e1
Jofre Tomas <jofretomas9@gmail.com>
2024-05-10 17:52:12 -0300

    Criação da API usando json

--- src/components/project/ProjectFrom.js
+++ src/components/project/ProjectFrom.js
@@ -1,9 +1,27 @@
+import { useEffect, useState } from "react";
+
 import Input from "../form/Input";
 import styles from "./ProjectFrom.module.css";
 import Select from "../form/Select";
 import SubmitButton from "../form/SubmitButton";
 
 function ProjectFrom({ btnText }) {
+  const [categories, setCategories] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:5000/categories", {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then((resp) => resp.json)
+      .then((data) => {
+        setCategories(data);
+      })
+      .catch((err) => console.log(err));
+  }, []);
+
   return (
     <form className={styles.form}>
       <Input
@@ -20,7 +38,11 @@ function ProjectFrom({ btnText }) {
         placeholder="Insira o orçamento total"
       />
 
-      <Select name="categoria_id" text="Selecione a categoria" />
+      <Select
+        name="categoria_id"
+        text="Selecione a categoria"
+        options={categories}
+      />
 
       <SubmitButton text={btnText} />
     </form>
